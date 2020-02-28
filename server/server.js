1c7dc4b246e63b4b20a0acdf670247331063c85b
Andy <xcessivepupil2@gmail.com>
2020-02-28 15:43:36 -0800

    Add api route to redirect to all 3 modules' load balancers and modify bundle addresses

--- server/server.js
+++ server/server.js
@@ -10,6 +10,46 @@ app.use(express.static(dirPath));
 
 const { routes } = require('../proxyconfig.json');
 
+app.get('/api/:id', (req, res) => {
+  const moreHomesPromise = 
+  axios.get(`${routes.moreHomes.address}/${req.params.id}`)
+    .then(data => {
+      console.log('RETRIEVED MOREHOMES DATA');
+      return data.data;
+    })
+    .catch(err => {
+        console.log('ERROR RETRIEVING MOREHOMES INFORMATION', err);
+        return err;
+  });
+
+  const reviewsPromise = 
+  axios.get(`${routes.reviews.address}/${req.params.id}`)
+  .then(data => {
+    console.log('RETRIEVED REVIEWS DATA');
+    return data.data;
+  })
+  .catch(err => {
+      console.log('ERROR RETRIEVING REVIEWS INFORMATION', err);
+      return err;
+  });
+
+  const listingsPromise = 
+  axios.get(`${routes.listings.address}/${req.params.id}`)
+  .then(data => {
+    console.log('RETRIEVED LISTINGS DATA');
+    return data.data;
+  })
+  .catch(err => {
+      console.log('ERROR RETRIEVING LISTINGS INFORMATION', err);
+      return err;
+  });
+
+  Promise.all([moreHomesPromise, reviewsPromise, listingsPromise])
+  .then((values) => { res.status(200).send(values) })
+  .catch(error => { res.status(500).send(error) });
+
+});
+
 app.get('/api/:component/:id', (req, res) => {
   axios.get(`${routes[req.params.component].address}/${req.params.id}`)
   .then(data => res.send(data.data))
